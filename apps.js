2e0783f4b55a35ea9b97643446df14c27b885005
brian babb <babb.brian@gmail.com>
2019-10-31 19:14:37 -0400

    axios installed

---
+++ apps.js
@@ -0,0 +1,14 @@
+var SpotifyWebApi = require('spotify-web-api-node');
+
+var spotifyApi = new SpotifyWebApi ({
+
+    Client ID 1649bc8344474f0ea495d350bef698c6
+    Client Secret 407ada8ae29947f59b1106a4907c79de 
+   
+
+});
+
+
+
+
+console.log(spotifyApi);
